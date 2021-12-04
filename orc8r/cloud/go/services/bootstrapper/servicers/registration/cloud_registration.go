eb58d87190e8cd483b26bc00fcf73ab430202f06
Regina Wang <rwang3495@gmail.com>
2021-12-03 16:12:16 -0800

    rebase fixes
    Signed-off-by: Regina Wang <rwang3495@gmail.com>

--- orc8r/cloud/go/services/bootstrapper/servicers/registration/cloud_registration.go
+++ orc8r/cloud/go/services/bootstrapper/servicers/registration/cloud_registration.go
@@ -19,18 +19,6 @@ import (
 	"github.com/golang/glog"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-<<<<<<< HEAD
-=======
-
-	"magma/orc8r/lib/go/protos"
-)
-
-const (
-	rootCAFilePath = "/var/opt/magma/certs/rootCA.pem"
-
-	// length of timeout for the nonce
-	timeoutDuration = 30 * time.Minute
->>>>>>> 44fa0eb5a (add config)
 
 	"magma/orc8r/cloud/go/clock"
 	"magma/orc8r/lib/go/protos"
@@ -40,15 +28,9 @@ const (
 const NotImplementedWarning = "warning: not implemented"
 
 type cloudRegistrationServicer struct {
-<<<<<<< HEAD
 	store   Store
 	rootCA  string
 	timeout time.Duration
-=======
-	store                    Store
-	rootCA                   string
-	timeoutDurationInMinutes int
->>>>>>> 44fa0eb5a (add config)
 }
 
 func NewCloudRegistrationServicer(store Store, rootCA string, timeout time.Duration) (protos.CloudRegistrationServer, error) {
@@ -140,16 +122,8 @@ func (c *cloudRegistrationServicer) generateAndSaveTokenInfo(networkID string, l
 			NetworkId: networkID,
 			LogicalId: logicalID,
 		},
-<<<<<<< HEAD
 		Nonce:   nonce,
 		Timeout: GetTimestamp(timeout),
-=======
-		Nonce: nonce,
-		Timeout: &timestamp.Timestamp{
-			Seconds: int64(t.Second()),
-			Nanos:   int32(t.Unix()),
-		},
->>>>>>> 44fa0eb5a (add config)
 	}
 
 	err := c.store.SetTokenInfo(tokenInfo)
